4e855f0f74fbba6320aeabaf574d7f3b49a2894e
KYLE YI SO <G00433001@atu.ie>
2025-06-18 23:13:56 +0100

    Update 02 ( added more UI, added in basic shooting pattern + the setup of weapons+ammo )

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,10 +2,22 @@
 {
     public partial class MainPage : ContentPage
     {
-        int CurrentPlayerPositionX=0, CurrentPlayerPositionY=0;
-        int RandomPositionX = 0, RandomPositionY = 0,rtime;
+        int CurrentPlayerPositionX=0, CurrentPlayerPositionY=0, BackgroundCurrentPositionX=0, BackgroundCurrentPositionY=0;
+        int RandomPositionX = 0, RandomPositionY = 0,rtime,projectilecycle01=0, weaponequipped=0;
         int ei1curposX, ei2curposX, ei3curposX, ei4curposX, ei5curposX, ei6curposX, ei7curposX, ei8curposX;
         int ei1curposY, ei2curposY, ei3curposY, ei4curposY, ei5curposY, ei6curposY, ei7curposY, ei8curposY;
+        int activeprojectilepositioni1X, activeprojectilepositioni1Y;
+        int activeprojectilepositioni2X, activeprojectilepositioni2Y;
+        int activeprojectilepositioni3X, activeprojectilepositioni3Y;
+        int activeprojectilepositioni4X, activeprojectilepositioni4Y;
+        int activeprojectilepositioni5X, activeprojectilepositioni5Y;
+        int activeprojectilepositioni6X, activeprojectilepositioni6Y;
+        int activeprojectilepositioni7X, activeprojectilepositioni7Y;
+        int activeprojectilepositioni8X, activeprojectilepositioni8Y;
+        int activeprojectilepositioni9X, activeprojectilepositioni9Y;
+        int activeprojectilepositioni10X, activeprojectilepositioni10Y;
+        int activeprojectilepositioni11X, activeprojectilepositioni11Y;
+        int ammunition01 = 0;
         Random RNGmove = new Random();
 
 
@@ -17,24 +29,47 @@
         {
             base.OnAppearing();
             await Task.Delay(200);
-            //hideallcontent();
+            hideallgamecontent();
             //testcontent1();
             Infinite_RNG_Movement();
             Update_All_Position_Constant();
+            await PlayerHPbar.TranslateTo(-98, 0, 40);
+            await PlayerStaminabar.TranslateTo(-93, 0, 40);
+            await PlayerMagicbar.TranslateTo(-102, 0, 40);
         }
-        async void hideallcontent() // for testing will be kept out of use FN
+        async void hideallgamecontent() // for testing will be kept out of use FN
         {
-            await PlayerIMG.FadeTo(0, 50);
-            await PlayerHitBox.FadeTo(0, 50);
-            await PlayerCameraBox.FadeTo(0, 50);
-            await ei1.FadeTo(0, 50);
-            await ei2.FadeTo(0, 50);
-            await ei3.FadeTo(0, 50);
-            await ei4.FadeTo(0, 50);
-            await ei5.FadeTo(0, 50);
-            await ei6.FadeTo(0, 50);
-            await ei7.FadeTo(0, 50);
-            await ei8.FadeTo(0, 50);
+            await BackgroundIMG.FadeTo(0, 5);
+            await PlayerIMG.FadeTo(0, 5);
+            await PlayerHitBox.FadeTo(0, 5);
+            await PlayerCameraBox.FadeTo(0, 5);
+            await ei1.FadeTo(0, 5);
+            await ei2.FadeTo(0, 5);
+            await ei3.FadeTo(0, 5);
+            await ei4.FadeTo(0, 5);
+            await ei5.FadeTo(0, 5);
+            await ei6.FadeTo(0, 5);
+            await ei7.FadeTo(0, 5);
+            await ei8.FadeTo(0, 5);
+            await Projectile01.FadeTo(0, 4);
+            await Projectile02.FadeTo(0, 4);
+            await Projectile03.FadeTo(0, 4);
+            await Projectile04.FadeTo(0, 4);
+            await Projectile05.FadeTo(0, 4);
+            await Projectile06.FadeTo(0, 4);
+            await Projectile07.FadeTo(0, 4);
+            await Projectile08.FadeTo(0, 4);
+            await Projectile09.FadeTo(0, 4);
+            await Projectile10.FadeTo(0, 4);
+            await Projectile11.FadeTo(0, 4);
+            await PlayerMagicbase.FadeTo(0, 5);
+            await PlayerMagicbar.FadeTo(0, 5);
+            await PlayerHPbase.FadeTo(0, 5);
+            await PlayerHPbar.FadeTo(0, 5);
+            await PlayerStaminabase.FadeTo(0, 5);
+            await PlayerStaminabar.FadeTo(0, 5);
+            await leftmovebutton.FadeTo(0, 5);
+            await attackbutton.FadeTo(0, 5);
         }
         async void testcontent1()
         {
@@ -57,6 +92,7 @@
 
 
         }
+        
         private void MoveBTN_Clicked(object sender, EventArgs e)
         {
             if (CurrentPlayerPositionY >=-290) {
@@ -64,8 +100,11 @@
                 Move_player_Hit_Box();
                 Move_player_Camera_Box();
             }
+            
             else
             {
+                // moves world to simulate moving through expanded world
+                BackgroundCurrentPositionY = BackgroundCurrentPositionY + 15;
                 ei1curposY = ei1curposY + 15;
                 ei2curposY = ei2curposY + 15; 
                 ei3curposY = ei3curposY + 15; 
@@ -78,10 +117,12 @@
                 
             }
         }
+
         async void Move_player()// split the 3 moving seperately so they all move at once together
         {
             CurrentPlayerPositionY=CurrentPlayerPositionY - 115;
-            await PlayerIMG.TranslateTo(CurrentPlayerPositionX, CurrentPlayerPositionY, 40); 
+            await PlayerIMG.TranslateTo(CurrentPlayerPositionX, CurrentPlayerPositionY, 40);
+            
 
         }
         async void Move_player_Hit_Box()
@@ -105,6 +146,174 @@
                 await Task.Delay(2000);
             }
         }
+        private void AttackBTN_Clicked(object sender, EventArgs e)
+        {
+            if (weaponequipped == 0)
+            {
+                if(ammunition01 != 0)
+                {
+                    if (projectilecycle01 <= 10)
+                    {
+                        projectilecycle01++;
+                    }
+                    else if (projectilecycle01 == 11)
+                    {
+                        projectilecycle01 = 1;
+                    }
+                    bullet_animation01();
+
+                }               
+            }
+        }
+        async void bullet_animation01()
+        {
+            switch (projectilecycle01)
+            {
+                case 1:
+                    await Projectile01.FadeTo(1, 1);
+                    activeprojectilepositioni1X = CurrentPlayerPositionX;
+                    activeprojectilepositioni1Y = CurrentPlayerPositionY;
+                    await Projectile01.TranslateTo(CurrentPlayerPositionX, CurrentPlayerPositionY, 1);
+                    for (int i = 0; i < 60; i++)
+                    {
+                        //activeprojectilepositionX= activeprojectilepositionX + 5;
+                        activeprojectilepositioni1Y = activeprojectilepositioni1Y - 8;
+                        await Projectile01.TranslateTo(activeprojectilepositioni1X, activeprojectilepositioni1Y, 1);
+                    }
+                    await Projectile01.FadeTo(0, 40);
+                    break;
+                case 2:
+                    await Projectile02.FadeTo(1, 1);
+                    activeprojectilepositioni2X = CurrentPlayerPositionX;
+                    activeprojectilepositioni2Y = CurrentPlayerPositionY;
+                    await Projectile02.TranslateTo(CurrentPlayerPositionX, CurrentPlayerPositionY, 1);
+                    for (int h = 0; h < 60; h++)
+                    {
+                        //activeprojectilepositionX= activeprojectilepositionX + 5;
+                        activeprojectilepositioni1Y = activeprojectilepositioni1Y - 8;
+                        await Projectile02.TranslateTo(activeprojectilepositioni2X, activeprojectilepositioni2Y, 1);
+                    }
+                    await Projectile02.FadeTo(0, 40);
+                    break;
+                case 3:
+                    await Projectile03.FadeTo(1, 1);
+                    activeprojectilepositioni3X = CurrentPlayerPositionX;
+                    activeprojectilepositioni3Y = CurrentPlayerPositionY;
+                    await Projectile03.TranslateTo(CurrentPlayerPositionX, CurrentPlayerPositionY, 1);
+                    for (int g = 0; g < 60; g++)
+                    {
+                        //activeprojectilepositionX= activeprojectilepositionX + 5;
+                        activeprojectilepositioni3Y = activeprojectilepositioni3Y - 8;
+                        await Projectile03.TranslateTo(activeprojectilepositioni3X, activeprojectilepositioni3Y, 1);
+                    }
+                    await Projectile03.FadeTo(0, 40);
+                    break;
+                case 4:
+                    await Projectile04.FadeTo(1, 1);
+                    activeprojectilepositioni4X = CurrentPlayerPositionX;
+                    activeprojectilepositioni4Y = CurrentPlayerPositionY;
+                    await Projectile04.TranslateTo(CurrentPlayerPositionX, CurrentPlayerPositionY, 1);
+                    for (int b = 0; b < 60; b++)
+                    {
+                        //activeprojectilepositionX= activeprojectilepositionX + 5;
+                        activeprojectilepositioni4Y = activeprojectilepositioni4Y - 8;
+                        await Projectile04.TranslateTo(activeprojectilepositioni4X, activeprojectilepositioni4Y, 1);
+                    }
+                    await Projectile04.FadeTo(0, 40);
+                    break;
+                case 5:
+                    await Projectile05.FadeTo(1, 1);
+                    activeprojectilepositioni5X = CurrentPlayerPositionX;
+                    activeprojectilepositioni5Y = CurrentPlayerPositionY;
+                    await Projectile05.TranslateTo(CurrentPlayerPositionX, CurrentPlayerPositionY, 1);
+                    for (int z = 0; z < 60; z++)
+                    {
+                        //activeprojectilepositionX= activeprojectilepositionX + 5;
+                        activeprojectilepositioni5Y = activeprojectilepositioni5Y - 8;
+                        await Projectile05.TranslateTo(activeprojectilepositioni5X, activeprojectilepositioni5Y, 1);
+                    }
+                    await Projectile05.FadeTo(0, 40);
+                    break;
+                case 6:
+                    await Projectile06.FadeTo(1, 1);
+                    activeprojectilepositioni6X = CurrentPlayerPositionX;
+                    activeprojectilepositioni6Y = CurrentPlayerPositionY;
+                    await Projectile06.TranslateTo(CurrentPlayerPositionX, CurrentPlayerPositionY, 1);
+                    for (int x = 0; x < 60; x++)
+                    {
+                        //activeprojectilepositionX= activeprojectilepositionX + 5;
+                        activeprojectilepositioni6Y = activeprojectilepositioni6Y - 8;
+                        await Projectile06.TranslateTo(activeprojectilepositioni6X, activeprojectilepositioni6Y, 1);
+                    }
+                    await Projectile06.FadeTo(0, 40);
+                    break;
+                case 7:
+                    await Projectile07.FadeTo(1, 1);
+                    activeprojectilepositioni7X = CurrentPlayerPositionX;
+                    activeprojectilepositioni7Y = CurrentPlayerPositionY;
+                    await Projectile07.TranslateTo(CurrentPlayerPositionX, CurrentPlayerPositionY, 1);
+                    for (int v = 0; v < 60; v++)
+                    {
+                        //activeprojectilepositionX= activeprojectilepositionX + 5;
+                        activeprojectilepositioni7Y = activeprojectilepositioni7Y - 8;
+                        await Projectile07.TranslateTo(activeprojectilepositioni7X, activeprojectilepositioni7Y, 1);
+                    }
+                    await Projectile07.FadeTo(0, 40);
+                    break;
+                case 8:
+                    await Projectile08.FadeTo(1, 1);
+                    activeprojectilepositioni8X = CurrentPlayerPositionX;
+                    activeprojectilepositioni8Y = CurrentPlayerPositionY;
+                    await Projectile08.TranslateTo(CurrentPlayerPositionX, CurrentPlayerPositionY, 1);
+                    for (int q = 0; q < 60; q++)
+                    {
+                        //activeprojectilepositionX= activeprojectilepositionX + 5;
+                        activeprojectilepositioni8Y = activeprojectilepositioni8Y - 8;
+                        await Projectile08.TranslateTo(activeprojectilepositioni8X, activeprojectilepositioni8Y, 1);
+                    }
+                    await Projectile08.FadeTo(0, 40);
+                    break;
+                case 9:
+                    await Projectile09.FadeTo(1, 1);
+                    activeprojectilepositioni9X = CurrentPlayerPositionX;
+                    activeprojectilepositioni9Y = CurrentPlayerPositionY;
+                    await Projectile09.TranslateTo(CurrentPlayerPositionX, CurrentPlayerPositionY, 1);
+                    for (int t = 0; t < 60; t++)
+                    {
+                        //activeprojectilepositionX= activeprojectilepositionX + 5;
+                        activeprojectilepositioni9Y = activeprojectilepositioni9Y - 8;
+                        await Projectile09.TranslateTo(activeprojectilepositioni9X, activeprojectilepositioni9Y, 1);
+                    }
+                    await Projectile09.FadeTo(0, 40);
+                    break;
+                case 10:
+                    await Projectile10.FadeTo(1, 1);
+                    activeprojectilepositioni10X = CurrentPlayerPositionX;
+                    activeprojectilepositioni10Y = CurrentPlayerPositionY;
+                    await Projectile10.TranslateTo(CurrentPlayerPositionX, CurrentPlayerPositionY, 1);
+                    for (int j = 0; j < 60; j++)
+                    {
+                        //activeprojectilepositionX= activeprojectilepositionX + 5;
+                        activeprojectilepositioni10Y = activeprojectilepositioni10Y - 8;
+                        await Projectile10.TranslateTo(activeprojectilepositioni10X, activeprojectilepositioni10Y, 1);
+                    }
+                    await Projectile10.FadeTo(0, 40);
+                    break;
+                case 11:
+                    await Projectile11.FadeTo(1, 1);
+                    activeprojectilepositioni11X = CurrentPlayerPositionX;
+                    activeprojectilepositioni11Y = CurrentPlayerPositionY;
+                    await Projectile11.TranslateTo(CurrentPlayerPositionX, CurrentPlayerPositionY, 1);
+                    for (int k = 0; k < 60; k++)
+                    {
+                        //activeprojectilepositionX= activeprojectilepositionX + 5;
+                        activeprojectilepositioni11Y = activeprojectilepositioni11Y - 8;
+                        await Projectile11.TranslateTo(activeprojectilepositioni11X, activeprojectilepositioni11Y, 1);
+                    }
+                    await Projectile11.FadeTo(0, 40);
+                    break;
+            }
+        }
         async void Reset_All_Enemy_Position()
         {
             await ei1.TranslateTo(0, 0, 40);
@@ -121,6 +330,7 @@
 
             while (true)
             {
+                await BackgroundIMG.TranslateTo(BackgroundCurrentPositionX, BackgroundCurrentPositionY, 40);
                 await ei1.TranslateTo(ei1curposX, ei1curposY, 40);
                 await ei2.TranslateTo(ei2curposX, ei2curposY, 40);
                 await ei3.TranslateTo(ei3curposX, ei3curposY, 40);
