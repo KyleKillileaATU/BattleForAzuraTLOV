73895fb9bc9f0142ff4e8c274c117c6b52532316
KYLE YI SO <G00433001@atu.ie>
2025-06-30 18:04:34 +0100

    Update 05 ( Big update overhauling many sistems added weapon menu, player movements, and improved multiple areas )

--- obj/Debug/net8.0-android/BattleForAzuraTLOV.AssemblyInfo.cs
+++ obj/Debug/net8.0-android/BattleForAzuraTLOV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BattleForAzuraTLOV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+203a1a7a3bffae0f390128cc9e83cc720989e408")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+adc79b509f16e6adb74ff177bbf48a7c051cdb5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BattleForAzuraTLOV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BattleForAzuraTLOV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
